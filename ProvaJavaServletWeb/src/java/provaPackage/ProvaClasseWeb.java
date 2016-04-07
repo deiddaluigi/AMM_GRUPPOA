d482e4f9ad026b426cbbc542a75a4c501af13c92
Luigi <Luigi@Luigi-PC>
2016-04-07 21:21:48 +0200

    Prova Java Web Application Servlet

---
+++ ProvaJavaServletWeb/src/java/provaPackage/ProvaClasseWeb.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package provaPackage;
+
+/**
+ *
+ * @author Luigi
+ */
+public class ProvaClasseWeb {
+    String visualizza;
+    
+    public ProvaClasseWeb (String visualizza){
+        this.visualizza = visualizza;
+    }
+    public String visualizzazione(){
+        return visualizza;     
+    }
+    
+}
---
+++ ProvaJavaServletWeb/src/java/provaPackage/VerificaUserid.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package provaPackage;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Luigi
+ */
+@WebServlet(name = "VerificaUserid", urlPatterns = {"/VerificaUserid"})
+public class VerificaUserid extends HttpServlet {
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet VerificaUserid</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Hai inserito il seguente user id:</h1>");
+            String nome = request.getParameter("userid");
+            if ( nome != null)
+                out.println("<p> " + nome + " </p>");
+            else 
+                out.println("<p> Non hai inserito nessun userid </p>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
