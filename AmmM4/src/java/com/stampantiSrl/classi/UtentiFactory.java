e671676929435eb020de36e6ddce3543a06ef54c
Luigi <Luigi@Luigi-PC>
2016-05-14 19:11:54 +0200

    Task 3 Milestone 4

--- AmmM4/src/java/com/stampantiSrl/classi/UtentiFactory.java
+++ AmmM4/src/java/com/stampantiSrl/classi/UtentiFactory.java
@@ -27,21 +27,21 @@ public class UtentiFactory {
     
     /**Costruisce alcune istanze di Clienti e una istanza Venditore in listaAccount, ed i rispettivi Conti Corrente*/
     private UtentiFactory(){
-        Cliente cliente1 = new Cliente("BNCMRA81A01A100A");
-        cliente1.setNomeCognome("Mario", "Bianchi");
-        cliente1.setUsername("MarioBianchi");
-        cliente1.setPassword("aaabbb");
-        listaAccount.add(cliente1);
-        Cliente cliente2 = new Cliente("MLNFRN82A02A200B");
-        cliente2.setNomeCognome("Franco", "Milani");
-        cliente2.setUsername("FrancoMilani");
-        cliente2.setPassword("cccddd");
-        listaAccount.add(cliente2);
-        Cliente cliente3 = new Cliente("MRNVRN83A03A300C");
-        cliente3.setNomeCognome("Veronica", "Marini");
-        cliente3.setUsername("VeronicaMarini");
-        cliente3.setPassword("eeefff");
-        listaAccount.add(cliente3);
+        Cliente cliente = new Cliente("BNCMRA81A01A100A");
+        cliente.setNomeCognome("Mario", "Bianchi");
+        cliente.setUsername("MarioBianchi");
+        cliente.setPassword("aaabbb");
+        listaAccount.add(cliente);
+        cliente = new Cliente("MLNFRN82A02A200B");
+        cliente.setNomeCognome("Franco", "Milani");
+        cliente.setUsername("FrancoMilani");
+        cliente.setPassword("cccddd");
+        listaAccount.add(cliente);
+        cliente = new Cliente("MRNVRN83A03A300C");
+        cliente.setNomeCognome("Veronica", "Marini");
+        cliente.setUsername("VeronicaMarini");
+        cliente.setPassword("eeefff");
+        listaAccount.add(cliente);
         Venditore venditore = new Venditore("StampantiSrl");
         venditore.setP_iva("01234567890");
         venditore.setUsername("stampantisrl");
